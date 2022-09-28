b3aa3ae2b5ffebeef82634b2b00cfceb667df711
Denis Maina <Denis.maina@student.moringaschool.com>
2022-09-28 20:45:35 +0300

    updated controller validations

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -10,7 +11,12 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(bird_params)
-    render json: bird, status: :created
+    if bird.valid?
+      render json: bird, status: :created
+    else
+      render json: { errors: bird.errors }, status: :unprocessable_entity
+    end
+    
   end
 
   # GET /birds/:id
